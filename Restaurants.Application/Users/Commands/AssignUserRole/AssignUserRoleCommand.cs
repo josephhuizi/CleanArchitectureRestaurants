c292c9fc351c41ef478f0d65ab6cd9e4a491d13d
Joseph Huizi <joseph.huizi@gmail.com>
2024-09-12 17:11:24 -0400

    Endpoint to assign roles to users

---
+++ Restaurants.Application/Users/Commands/AssignUserRole/AssignUserRoleCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application.Users.Commands.AssignUserRole;
+
+public class AssignUserRoleCommand : IRequest
+{
+	public required string UserEmail { get; set; }
+	public required string RoleName { get; set; }
+}
---
+++ Restaurants.Application/Users/Commands/AssignUserRole/AssignUserRoleCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Application.Users.Commands.AssignUserRole;
+
+internal class AssignUserRoleCommandHandler(ILogger<AssignUserRoleCommandHandler> logger,
+	UserManager<User> userManager, RoleManager<IdentityRole> roleManager) : IRequestHandler<AssignUserRoleCommand>
+{
+	public async Task Handle(AssignUserRoleCommand request, CancellationToken cancellationToken)
+	{
+		logger.LogInformation("Assigning user role: {@request}", request);
+
+		var user = await userManager.FindByEmailAsync(request.UserEmail) ?? 
+			throw new NotFoundException("Not found user");
+
+		var role = await roleManager.FindByNameAsync(request.RoleName) ?? 
+			throw new NotFoundException("The role doesn't exist");
+
+		await userManager.AddToRoleAsync(user, role.Name!);
+
+
+
+	}
+}
